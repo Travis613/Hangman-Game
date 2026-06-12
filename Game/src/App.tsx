7b5a98e9b81be79ebe7e793ba8b3924e8cb42366
Travis <travism.github@gmail.com>
2026-06-12 19:24:35 -0400

    Made the hanging stand

--- Game/src/App.tsx
+++ Game/src/App.tsx
@@ -1,8 +1,16 @@
 import { randomWords } from "../public/util";
 
 export default function App() {
-  return;
-  <div>
-    <div>Hangman Game</div>
-  </div>;
+  return (
+    <div className="documentBody">
+      <div className="stickFigureSection">
+        <div className="hangingStand">
+          <div className="horizontalTopPost" />
+          <div className="verticlePost" />
+          <div className="rope" />
+          <div className="horizontalBottomPost" />
+        </div>
+      </div>
+    </div>
+  );
 }
