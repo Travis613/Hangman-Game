6eadc20fbf631c62353d86465ec859fa5707b433
Travis <travism.github@gmail.com>
2026-06-14 17:05:29 -0400

    Refactored redunant css

--- Game/src/App.tsx
+++ Game/src/App.tsx
@@ -3,19 +3,19 @@
 export default function App() {
   return (
     <div className="documentBody">
-      <div className="stickFigureSection">
+      <div className="hangindSectionWithStickFigure">
         <div className="hangingStand">
-          <div className="horizontalTopPost" />
-          <div className="verticlePost" />
-          <div className="rope" />
-          <div className="horizontalBottomPost" />
+          <div className="horizontalTopPost redunantStyles" />
+          <div className="verticlePost redunantStyles" />
+          <div className="rope redunantStyles" />
+          <div className="horizontalBottomPost redunantStyles" />
           <div className="stickManSection">
-            <div className="head" />
-            <div className="torso" />
-            <div className="leftArm" />
-            <div className="rightArm" />
-            <div className="leftLeg" />
-            <div className="rightLeg" />
+            <div className="head redunantStyles" />
+            <div className="torso redunantStyles" />
+            <div className="leftArm redunantStyles" />
+            <div className="rightArm redunantStyles" />
+            <div className="leftLeg redunantStyles" />
+            <div className="rightLeg redunantStyles" />
           </div>
         </div>
       </div>
